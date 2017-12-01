f810db9793a08716ce81a9b1fa2e85a199ca5cae
Cwander83 <chriswandermail@gmail.com>
2017-12-01 16:24:27 -0500

    12-1-17(first button running)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,50 @@
-console.log("hiiiiiiiiiii");
+//global vars to use
+
+//on click function for gifs to load on page
+$("button").on("click", function () {
+    //
+    var nfl = $(this).attr("data-nfl")
+
+    //the api key from giphy
+    //====wdOCXpHvWDf9a3U3UbXR23FkuRdyHXpA
+    var queryURL = "https://api.giphy.com/v1/gifs/search";
+
+    //the ajax connector
+    $.ajax({
+            url: queryURL,
+            method: 'GET',
+            data: {
+                q: nfl,
+                api_key: "wdOCXpHvWDf9a3U3UbXR23FkuRdyHXpA",
+                limit: 9,
+            }
+        })
+        .done(function (response) {
+            console.log(response);
+            var result = response.data;
+            
+
+            $("#nfl-teams").empty();
+
+            for (var i = 0; i < result.length; i++){
+
+                gifDiv = $("<div class='gifdiv'>");
+
+                var rating = result[i].rating;
+
+                var p = $("<p>").text("Rating: " + rating);
+
+                var image = $("<img>");
+
+                image.attr("src", result[i].images.fixed_height.url);
+
+                gifDiv.prepend(p);
+                gifDiv.prepend(image);
+
+                $("#nfl-teams").prepend(gifDiv);
+
+            }
+
+        });
+        console.log(this);
+});
